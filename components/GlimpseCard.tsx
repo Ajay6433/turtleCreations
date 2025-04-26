ad512fe0b22df4a02521a1b4070adbeb1eb19567
Ajay Khan <Ajaykhan6433@gmail.com>
2025-04-26 20:57:02 +0530

    added box shadow to the glimpse card

--- components/GlimpseCard.tsx
+++ components/GlimpseCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface GlimpseCardProps {
   title: string;
@@ -8,19 +9,22 @@ interface GlimpseCardProps {
 
 const GlimpseCard = ({ title, description, imgSrc }: GlimpseCardProps) => {
   return (
-    <div className="box-border w-full max-w-[536px] min-h-[300px] bg-white border-1 mb-8 mx-auto">
+    <div className="box-border w-full max-w-[536px] min-h-[300px] bg-white border-1 mb-8 mx-auto shadow-[-4px_6px_1px_rgba(0,0,0,0.25)]">
       <div className="w-full h-full flex flex-col items-center p-4 relative">
         {/* Border elements */}
         <div className="w-[calc(100%+16px)] h-[4px] rounded-2xl bg-black absolute top-0 -left-2"></div>
-        <div className="w-[4px] h-[calc(100%+16px)] rounded-2xl bg-black absolute left-0 -top-2"></div>
+        <div className="w-[4px] h-[calc(100%+16px)] rounded-2xl bg-black absolute left-0 -top-2 shadow-[-6px_4px_1px_rgba(0,0,0,0.25)]"></div>
         <div className="w-[4px] h-[calc(100%+16px)] rounded-2xl bg-black absolute right-0 -top-2"></div>
-        <div className="w-[calc(100%+16px)] h-[4px] rounded-2xl bg-black absolute bottom-0 -left-2"></div>
+        <div className="w-[calc(100%+16px)] h-[4px] rounded-2xl bg-black absolute bottom-0 -left-2 shadow-[-4px_6px_1px_rgba(0,0,0,0.25)]"></div>
 
         {/* Image */}
-        <img
+        <Image
           src={imgSrc}
           alt="Glimpse"
+          width={463}
+          height={350}
           className="w-full max-w-[463px] h-[200px] sm:h-[250px] md:h-[350px] object-cover border-2"
+          loading="lazy"
         />
 
         {/* Content */}
--- components/GlimpseHeading.tsx
+++ components/GlimpseHeading.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const GlimpseHeading = () => {
     return (
@@ -6,6 +8,7 @@ const GlimpseHeading = () => {
                 <div>
                         <h2 className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-[64px] text-black font-semibold">
                                 Some Glimpse
+                                <Image src="/images/glimpse.png" alt="glimpseHeading" width={200} height={50} className="inline-block ml-2" />
                         </h2>
                 </div>
         </div>
